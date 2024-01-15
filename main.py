ecfc7b0b1d9d83ded8f8f091866e79d50dcd3e5e
vanischesmall <vanischesmall@gmail.com>
2024-01-15 19:04:20 +1000

    create

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from packaging import Package as pkg
 
 
 if __name__ == '__main__':
-    values: list = list(map(int, input("Enter values: ").split()))
-    string_from_values = pkg.encode(values)
+    values: list[int] = list(map(int, input("Enter values: ").split()))
+    string_from_values: str = pkg.encode(values)
     print(f'Encoded package string -> {string_from_values}\n')
     
     string: str = str(input("Enter package string: "))
-    values_from_string  = pkg.decode(string)
+    values_from_string: list[int]  = pkg.decode(string)
     print(f'Decoded list of values -> {values_from_string}\n')      
